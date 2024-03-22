bd367823fecaa0912ce7d83e658588a929768476
avrubio <ariadna.vanegas@gmail.com>
2024-03-22 19:38:46 -0400

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    colors: {
+      black: "#000000",
+      burlywood: "#deb887",
+    },
+    extend: {
+      margin: {
+        50: "50px", // Adjust the size as needed
+      },
+    },
+  },
+};
